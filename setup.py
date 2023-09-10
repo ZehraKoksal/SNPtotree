99469cd266842010de0c58dbfdc53da420216dea
ZehraKoksal <128804427+ZehraKoksal@users.noreply.github.com>
2023-09-10 14:49:21 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='SNPtotree',
+    version='1.0',
+    author='Zehra Köksal',
+    description='A software to resolve the phylogeny of haploid SNPs',
+    url='https://github.com/ZehraKoksal/SNPtotree',
+    keywords='phylogeny, SNPs, haploid')
+ #   python_requires='>=3.7, <4',
+    #packages=['numpy'])
